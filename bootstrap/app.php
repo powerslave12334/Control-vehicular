58b58d544e40dd9ce94e447ccc544f801c554592
RicardoDev <desarrollo.sw1@aguainmaculada.info>
2026-08-06 16:27:35 -0600

    Trust Vercel proxy headers: HTTPS asset URLs (evita bloqueo CSP por esquema http)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_PREFIX
+                | Request::HEADER_X_FORWARDED_AWS_ELB,
+        );
+
         $middleware->api(prepend: [
             EnsureFrontendRequestsAreStateful::class,
         ]);
